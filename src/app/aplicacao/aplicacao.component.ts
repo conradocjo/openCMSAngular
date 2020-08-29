888f008a9993f4f6beeb88fb98d07c1fa639c5f3
conrado <conradocjo@gmail.com>
2020-08-29 20:38:32 -0300

    Evoluçao do front

---
+++ src/app/aplicacao/aplicacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cms-aplicacao',
+  templateUrl: './aplicacao.component.html',
+  styleUrls: ['./aplicacao.component.css']
+})
+export class AplicacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
